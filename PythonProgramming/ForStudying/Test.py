b3a579c221ab5b16dda63d6aebf6cb8ff1ec0ac1
Bochog <johnmavarlez@gmail.com>
2024-12-11 01:26:31 +0800

    Test Project

--- PythonProgramming/ForStudying/Test.py
+++ PythonProgramming/ForStudying/Test.py
@@ -1,11 +1,10 @@
-import json
-
-mystring = '[{"name": "The Matrix", "artist": "Keanu Reeves"}]'
-stringtodict = json.loads(mystring)
-print(stringtodict)
-
-
-
-
-
-
+val=-1
+while val < 0:
+    try:
+        val=int(input('Enter first number: '))
+    except ValueError:
+        print("Enter an integer number!")
+    except TypeError:
+        print("Enter an integer number!")
+import sys
+print(sys.path)
---
+++ PythonSelenium/Google/Elements/GoogleElements.py
@@ -0,0 +1,7 @@
+class GoogleElement:
+    def __init__(self):
+        self.button_gmail = '//*[@id="yDmH0d"]/c-wiz/div/div/c-wiz/div/div/div/div[2]/div[1]/ul/li[6]/a/div/span'
+        self.button_googleapps = "//a[@href='https://www.google.com.ph/intl/en/about/products']"
+        self.button_googleapps_gmail = "//a[@href='https://mail.google.com/mail/']"
+        self.iframe_googlehome = "//iframe[@name='app']"
+        self.button_workspace_signin = "//div[@class='header__aside']/a/span[text()='Sign in']"
